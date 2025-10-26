d742531fecbfc6b41539358be8938bc3272b9ca5
spencer-lukey <slukey@ualberta.ca>
2025-10-25 19:35:54 -0600

    Added CustomListTest test for adding items

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -5,12 +5,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //import org.junit.Before;
+import android.content.Context;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 public class CustomListTest {
+    private Context mockContext;
 
+    private CustomList mockList() {
+        return new CustomList(mockContext, new ArrayList<City>());
+    }
+
+    @Test
+    public void testCustomListAdd() {
+        // Create new CustomList
+        CustomList customList = mockList();
+
+        // Check there's no items in list
+        assertEquals(0, customList.getCount());
+
+        // Add new item
+        customList.add(new City("New York", "NY"));
+
+        // Check item was added
+        assertEquals(1, customList.getCount());
+    }
 
 }
